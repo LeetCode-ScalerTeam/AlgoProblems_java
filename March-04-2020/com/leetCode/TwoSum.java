50b658722aa808a1c7eb3bdaf290b5ea957ba9d4
Sundara Raghavan <sunragav@gmail.com>
2020-03-04 20:27:06 +0100

    March 4 2020- TwoSum

--- March-04-2020/com/leetCode/TwoSum.java
+++ March-04-2020/com/leetCode/TwoSum.java
@@ -1,26 +1,16 @@
-package com.leetCode;
-
-import java.util.*;
-
-class TwoSumsSolution {
+class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> seenMap = new HashMap<>();
-        int res[] = new int[2];
-        for(int i = 0 ; i < nums.length ; i++){
-            int temp = target - nums[i];
-            //System.out.println("temp -> "+temp+" seenMap.containsKey(temp) ->  "+seenMap.containsKey(temp));
-            if(seenMap.containsKey(temp)){
-                //System.out.println("In If");
-                res[0] = seenMap.get(temp);
-                res[1] = i;
-                return res;
-            }
-            else{
-                //System.out.println("else ");
-                seenMap.put(nums[i],i);
-            }
-            //System.out.println(seenMap);
+        Map<Integer,Integer> prefixSum = new HashMap<>();
+        int i=0;
+        for(int e:nums){
+            prefixSum.put( target-e,i++);
         }
-        return res;
+        i=0;
+        for(int e:nums){
+            Integer j=prefixSum.get(e);
+            if(j!=null && j!=i)return new int[]{Math.min(j,i),Math.max(j,i)};
+            i++;
+        }
+        return null;
     }
 }
