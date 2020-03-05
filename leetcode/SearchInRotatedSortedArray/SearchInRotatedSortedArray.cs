4dc43678343c442f5fea340379e8023b19151cea
tabaraq <tabaraq.sms@gmail.com>
2020-03-05 13:57:21 +0530

    commit

---
+++ leetcode/SearchInRotatedSortedArray/SearchInRotatedSortedArray.cs
@@ -0,0 +1,69 @@
+/*
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+You may assume no duplicate exists in the array.
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+-- Method Followed --
+ 
+To use Binary Search on the Rotated Sorted Array, it is necessary to update left and right pointers.
+Check if the target element is on which side and depending on that update left or right pointers.
+
+-- Complexity Analysis -- 
+
+Time complexity : O(logn)
+Space complexity : O(1)
+
+*/
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if (target == nums[mid])
+            {
+                return mid;
+            }
+            if (nums[left] <= nums[mid])
+            {
+                if (nums[left] <= target && target < nums[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            else
+            {
+                if (nums[mid] < target && target <= nums[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
