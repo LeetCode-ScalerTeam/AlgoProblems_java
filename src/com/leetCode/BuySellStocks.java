fd7bc854384789358428e3f45864ea1e9e9740de
Chandrakant <chandrakant.kh13@gmail.com>
2020-03-05 12:44:38 +0530

    Arrays-03-05-2019

--- src/com/leetCode/BuySellStocks.java
+++ src/com/leetCode/BuySellStocks.java
@@ -1,17 +1,11 @@
 package com.leetCode;
 class BuySellStocksSolution {
     public int maxProfit(int[] prices) {
-        if(prices.length <= 0)
-                return 0;
-        int max = prices[prices.length -1] , diffMax =0;
-        //System.out.println("max "+max +" diffMax "+diffMax);
-        for(int i = prices.length-2 ; i >= 0 ; i--){
-            if(prices[i] <= max && diffMax < (max - prices[i]))
-                diffMax = max - prices[i];
-            else if(prices[i] > max)
-                max = prices[i];
-            //System.out.println("max "+max +" prices[i]=> "+prices[i]+" diffMax "+diffMax);
+        int min = Integer.MAX_VALUE, max = 0, n = prices.length;
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, prices[i]);
+            max = Math.max(max, prices[i] - min);
         }
-        return diffMax;
+        return max;
     }
 }
